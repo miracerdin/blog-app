664643e9523396a3f443c265457a85c7a0d41fc0
mirac <miracerdin@gmail.com>
2022-08-05 18:41:17 +0300

    some detail page changes

--- src/pages/BlogDetail.jsx
+++ src/pages/BlogDetail.jsx
@@ -20,66 +20,75 @@ const BlogDetail = () => {
   const navigate = useNavigate();
   const [showMore, setShowMore] = React.useState(false);
   return (
-    <Card sx={{ width: "250px", m: "1rem" }}>
+    <div
+      style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+    >
       <CardMedia
         component="img"
-        height="194"
+        height="250px"
+        sx={{ width: "50%", mt: "3rem" }}
         image={state.post.imageUrl ? state.post.imageUrl : defaultImage}
         alt="Paella dish"
       />
-      <CardHeader title={state.post.title} subheader="September 14, 2016" />
-      <CardContent>
-        <Typography
-          variant="body2"
-          color="text.secondary"
-          height="90px"
-          sx={{
-            wordBreak: "break-word",
-            // textOverflow: "ellipsis",
-            // display: "-webkit-box",
-            // WebkitLineClamp: "2",
-            // WebkitBoxOrient: "vertical",
-            maxHeight: 200,
-            overflow: "auto",
-          }}
-        >
-          {!showMore ? state.post.content.slice(0, 100) : state.post.content}
-          <Box
-            component="span"
-            onClick={() => setShowMore(!showMore)}
-            sx={{ cursor: "pointer", color: "black" }}
+      <Card sx={{ width: "350px", m: "1rem" }}>
+        <CardHeader title={state.post.title} subheader="September 14, 2016" />
+        <CardContent>
+          <Typography
+            variant="body2"
+            color="text.secondary"
+            height="90px"
+            sx={{
+              wordBreak: "break-word",
+              // textOverflow: "ellipsis",
+              // display: "-webkit-box",
+              // WebkitLineClamp: "2",
+              // WebkitBoxOrient: "vertical",
+              maxHeight: 200,
+              overflow: "auto",
+            }}
           >
-            {state.post.content.length <= 150
-              ? ""
-              : !showMore
-              ? "...read more"
-              : " show less"}
-          </Box>
-        </Typography>
-      </CardContent>
-      <Box sx={{ display: "flex", alignItems: "center" }}>
-        <Avatar sx={{ bgcolor: "black" }} aria-label="recipe"></Avatar>
-        <Typography variant="body1" color="text.secondary" sx={{ ml: "1rem" }}>
-          {state.post.author}
-        </Typography>
-      </Box>
+            {!showMore ? state.post.content.slice(0, 100) : state.post.content}
+            <Box
+              component="span"
+              onClick={() => setShowMore(!showMore)}
+              sx={{ cursor: "pointer", color: "black" }}
+            >
+              {state.post.content.length <= 150
+                ? ""
+                : !showMore
+                ? "...read more"
+                : " show less"}
+            </Box>
+          </Typography>
+        </CardContent>
+        <Box sx={{ display: "flex", alignItems: "center" }}>
+          <Avatar sx={{ bgcolor: "black" }} aria-label="recipe"></Avatar>
+          <Typography
+            variant="body1"
+            color="text.secondary"
+            sx={{ ml: "1rem" }}
+          >
+            {state.post.author}
+          </Typography>
+        </Box>
 
-      <CardActions disableSpacing sx={{ justifyContent: "space-between" }}>
-        <IconButton aria-label="add to favorites">
-          <FavoriteIcon
-            sx={{ color: state.post.favourite !== 0 ? "red" : "dark" }}
-          />
-          <Box variant="body2">{state.post.favourite}</Box>
-        </IconButton>
-        <Button
-          variant="contained"
-          onClick={() => navigate(-1)}
-          // !currentUser && toastWarnNotify("Please log in to see the detail");
-        >
-          Back
-        </Button>
-      </CardActions>
-    </Card>
+        <CardActions disableSpacing sx={{ justifyContent: "space-between" }}>
+          <IconButton aria-label="add to favorites">
+            <FavoriteIcon
+              sx={{ color: state.post.favourite !== 0 ? "red" : "dark" }}
+            />
+            <Box variant="body2">{state.post.favourite}</Box>
+          </IconButton>
+          <Button
+            variant="contained"
+            onClick={() => navigate(-1)}
+            // !currentUser && toastWarnNotify("Please log in to see the detail");
+          >
+            Back
+          </Button>
+        </CardActions>
+      </Card>
+    </div>
   );
 };
 
