1febbecc9a49b51c6100eaf46a6d3ceb28024a36
mirac <miracerdin@gmail.com>
2022-08-05 19:07:30 +0300

    avatar added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,47 +5,28 @@ import Toolbar from "@mui/material/Toolbar";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 import Menu from "@mui/material/Menu";
-// import MenuIcon from "@mui/icons-material/Menu";
 import Container from "@mui/material/Container";
 import Avatar from "@mui/material/Avatar";
-// import Button from "@mui/material/Button";
 import Tooltip from "@mui/material/Tooltip";
 import MenuItem from "@mui/material/MenuItem";
-// import AdbIcon from "@mui/icons-material/Adb";
 import cw from "../assets/cw.jpeg";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../context/AuthContext";
 import { logOut } from "../auth/firebase";
 
-// const pages = ["Products", "Pricing", "Blog"];
-// const settings = ["login", "register", "Logout"];
-
 const Navbar = () => {
   const navigate = useNavigate();
   const { currentUser } = React.useContext(AuthContext);
-
   const [anchorElUser, setAnchorElUser] = React.useState(null);
 
-  //   const handleOpenNavMenu = (event) => {
-  //     setAnchorElNav(event.currentTarget);
-  //   };
   const handleOpenUserMenu = (event) => {
     setAnchorElUser(event.currentTarget);
   };
 
-  //   const handleCloseNavMenu = () => {
-  //     setAnchorElNav(null);
-  //   };
-
   const handleCloseUserMenu = () => {
     setAnchorElUser(null);
   };
-
-  //   const handleClick = (e) => {
-  //     navigate(`/${e.target.name}`);
-  //     console.log(e.target.name);
-  //   };
 
   return (
     <AppBar position="static">
@@ -62,62 +43,7 @@ const Navbar = () => {
             alt="The house from the offer."
             src={cw}
           />
-          {/* <AdbIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} /> */}
-          {/* <Typography
-            variant="h6"
-            noWrap
-            component="a"
-            href="/"
-            sx={{
-              mr: 2,
-              display: { xs: "none", md: "flex" },
-              fontFamily: "monospace",
-              fontWeight: 700,
-              letterSpacing: ".3rem",
-              color: "inherit",
-              textDecoration: "none",
-            }}
-          >
-            LOGO
-          </Typography> */}
 
-          {/* <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}> */}
-          {/* <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton> */}
-          {/* <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: "bottom",
-                horizontal: "left",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "left",
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: "block", md: "none" },
-              }}
-            >
-              {pages.map((page) => (
-                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  <Typography textAlign="center">{page}</Typography>
-                </MenuItem>
-              ))}
-            </Menu> */}
-          {/* </Box> */}
-          {/* <AdbIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} /> */}
           <Box
             sx={{
               flexGrow: 1,
@@ -179,7 +105,10 @@ const Navbar = () => {
           <Box sx={{ flexGrow: 0 }}>
             <Tooltip title="Open settings">
               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                <Avatar
+                  alt="Remy Sharp"
+                  src="https://avatars.githubusercontent.com/u/99042499?v=4"
+                />
               </IconButton>
             </Tooltip>
             <Menu
@@ -200,14 +129,7 @@ const Navbar = () => {
             >
               {currentUser ? (
                 <>
-                  <MenuItem
-                    onClick={handleCloseUserMenu}
-
-                    // sx={{
-                    //   cursor: "auto",
-                    //   "&:hover": { background: "transparent" },
-                    // }}
-                  >
+                  <MenuItem onClick={handleCloseUserMenu}>
                     <Typography
                       textAlign="center"
                       onClick={() => navigate("/profile")}
