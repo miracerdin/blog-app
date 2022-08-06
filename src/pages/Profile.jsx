8119d23a7f4e4a772ac3a3e5a2269acc3bf7159e
mirac <miracerdin@gmail.com>
2022-08-06 16:53:09 +0300

    styles added

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,11 +1,21 @@
 import Typography from "@mui/material/Typography";
 import React from "react";
 import Typewriter from "typewriter-effect";
-// import MovingComponent from "react-moving-text";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { Box } from "@mui/system";
 import Carousell from "../components/Carousel";
 const Profile = () => {
-  const Letters = ["mirac erdin hello"];
+  const theme = createTheme();
+
+  theme.typography.h2 = {
+    fontSize: "1.2rem",
+    "@media (min-width:600px)": {
+      fontSize: "1.5rem",
+    },
+    [theme.breakpoints.up("md")]: {
+      fontSize: "2.4rem",
+    },
+  };
   return (
     <Box
       sx={{
@@ -18,49 +28,37 @@ const Profile = () => {
         alignItems: "center",
       }}
     >
-      <Typography variant="h2" gutterBottom sx={{ color: "black" }}>
-        <Typewriter
-          // options={{
-          //   strings: ["Hello"],
-          //   autoStart: true,
-          //   loop: false,
-          // }}
-          onInit={(typewriter) => {
-            typewriter
-              .typeString("Hello")
-              .pauseFor(1000)
-              .deleteAll()
-              .typeString("My name is Miraç")
-              .start();
-          }}
-        />
-      </Typography>
-      <Typography variant="h2" gutterBottom sx={{ color: "black" }}>
-        <Typewriter
-          // options={{
-          //   delay: "250",
-          //   autoStart: true,
-          //   loop: false,
-          // }}
-          onInit={(typewriter) => {
-            typewriter.typeString("I am a Frontend Developer ").start();
-          }}
-        />
-      </Typography>
-
-      {/* {Letters.map((letter, index) => (
-        <MovingComponent
-          type="fadeInFromLeft"
-          duration="1000ms"
-          delay="index * 100ms"
-          direction="normal"
-          timing="ease"
-          iteration="1"
-          fillMode="forwards"
-        >
-          {letter}
-        </MovingComponent>
-      ))} */}
+      <ThemeProvider theme={theme}>
+        <Typography variant="h2" gutterBottom sx={{ color: "black" }}>
+          <Typewriter
+            // options={{
+            //   strings: ["Hello"],
+            //   autoStart: true,
+            //   loop: false,
+            // }}
+            onInit={(typewriter) => {
+              typewriter
+                .typeString("Hello")
+                .pauseFor(1000)
+                .deleteAll()
+                .typeString("My name is Miraç")
+                .start();
+            }}
+          />
+        </Typography>
+        <Typography variant="h2" gutterBottom sx={{ color: "black" }}>
+          <Typewriter
+            // options={{
+            //   delay: "250",
+            //   autoStart: true,
+            //   loop: false,
+            // }}
+            onInit={(typewriter) => {
+              typewriter.typeString("I am a Frontend Developer ").start();
+            }}
+          />
+        </Typography>
+      </ThemeProvider>
       <Carousell />
     </Box>
   );
